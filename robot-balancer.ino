be0f806fbceb6fea580f0698f35c4fa966bfd6cc
Aleh Zamzhytski <Aleh_Zamzhytski@epam.com>
2019-09-24 21:03:46 +0300

    Other IMU lib has been added

--- robot-balancer.ino
+++ robot-balancer.ino
@@ -1,49 +1,136 @@
-#include <Arduino.h>
-#include <AutoPID.h>
-#include "IMU6050.h"
+#include <MPU6050_6Axis_MotionApps20.h>
 #include "L298N.h"
+#include <AutoPID.h>
 
 #define LED_PIN 13
 #define OUTPUT_MIN -255
 #define OUTPUT_MAX 255
-#define MAX_DIF 30
-#define KP 20
-#define KI 15
-#define KD 32
 
-//TODO add configurable pins
+#define MAX_DIF 40
+#define KP 6
+#define KD 1.2
+#define KI 0.75
+
 DriveSystem *driveSystem = new DriveSystem(new Motor(6, 7, 8),
-        new Motor(11, 10, 9));
-IMU6050 *imu;
+    new Motor(11, 10, 9));
 
 double angle = 0;
 double target = 0;
 double speed = 0;
+Quaternion q;           // [w, x, y, z]         quaternion container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+VectorFloat gravity;    // [x, y, z]            gravity vector
+bool dmpReady = false;  // set true if DMP init was successful
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
 
+MPU6050 mpu;
 AutoPID myPID = AutoPID(&angle, &target, &speed, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 
 void setup() {
-	pinMode(13, OUTPUT);
-	digitalWrite(LED_PIN, 0);
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+  pinMode(13, OUTPUT);
+  digitalWrite(LED_PIN, 0);
   Serial.begin(19200);
-  Serial.println("calibration start");
-  imu = new IMU6050();
+  Serial.println("start");
+  mpu.initialize();
+
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
   myPID.setBangBang(MAX_DIF);
   myPID.setTimeStep(5);
-  Serial.println("calibration end");
-	digitalWrite(LED_PIN, 1);
+  Serial.println("connected");
+  digitalWrite(LED_PIN, 1);
 }
 
 void loop() {
-  imu->process();
-  angle = imu->get_gyro_y_angle();
-  myPID.run();
-  driveSystem->setSpeed(-speed, -speed);
-  Serial.print(angle);
-  Serial.print(" = ");
-  Serial.print(speed);
-  Serial.print(" ");
-  Serial.println(imu->get_last_process_time());
-  digitalWrite(LED_PIN, myPID.atSetPoint(2));
-  delay(5);
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+  if (fifoCount < packetSize) {
+    //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+    // This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+  }
+  // check for overflow (this should never happen unless our code is too inefficient)
+  else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+
+    // read a packet from FIFO
+    while (fifoCount >= packetSize) { // Lets catch up to NOW, someone is using the dreaded delay()!
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+    }
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    //    Serial.print("ypr\t");
+    //    Serial.print(ypr[0] * 180 / M_PI);
+    //    Serial.print("\t");
+    //    Serial.print(ypr[1] * 180 / M_PI);
+    //    Serial.print("\t");
+    //    Serial.println(ypr[2] * 180 / M_PI);
+    angle = ypr[1] * 180 / M_PI;
+
+    Serial.print(angle);
+    Serial.print(" - ");
+    Serial.print(speed);
+    Serial.print(fifoCount);
+    Serial.println(" : ");
+    myPID.run();
+    driveSystem->setSpeed(speed, speed);
+    digitalWrite(LED_PIN, myPID.atSetPoint(2));
+    delay(10);
+  }
 }
